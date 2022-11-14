65b96b6b97135e24cf29452b15af79c9bab88d1d
D-One <hsjo920914@gmail.com>
2022-11-14 10:33:45 +0900

    add ether/klay units

--- Chapter1/myToken.sol
+++ Chapter1/myToken.sol
@@ -5,8 +5,19 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contr
 
 /*
     Decimal : 18 
+    10 Token = 10^19 = 10000000000000000000 
     1 Token = 10^18 = 1000000000000000000 
     0.1 Token = 10^17 = 100000000000000000
+
+    이더리움
+    1 Ether = 10^18 = 1000000000000000000 wei
+    1 Gwei = 10^9 = 10000000 wei
+    1 Wei = 1 wei
+
+    클레이튼
+    1 Klay = 10^18 = 1000000000000000000 peb
+    1 Gpeb = 10^9 = 10000000 peb
+    1 Peb = 1 peb
 */
 
 contract myToken is ERC20Burnable, Ownable {
